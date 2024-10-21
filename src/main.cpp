647530519e8a7c4f546b3fb488e5d11ee9bb4b55
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-10-21 14:51:58 +0800

    Setup simple window of raylib to set up game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include "env/constant.h"
 #include <raylib.h>
-
-int main() {}
+int main() {
+  Color darkBlue = {44, 44, 127, 255};
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
+  /* Defination of game loop
+  1. Event Hnadling
+  2. Updating Postions
+  3. Drawing Objects
+  */
+  // Set speed of game loop, if not set it will run on computer clock cycle
+  SetTargetFPS(GAME_SPEED);
+  while (WindowShouldClose() == false) {
+    BeginDrawing();
+    ClearBackground(darkBlue);
+    EndDrawing();
+  }
+  CloseWindow();
+}
