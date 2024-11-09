9d1dd078266ed8f9c26035b141d6b378ff03ab33
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-11-09 17:34:55 +0800

    Write first test case for project

--- tests/src/ColorTest.cpp
+++ tests/src/ColorTest.cpp
@@ -1,1 +1,17 @@
+#include "Color.h"
+#include "raylib.h"
 #include <gtest/gtest.h>
+#include <vector>
+
+TEST(attribute_test, color_test) {
+  auto allColor = getCellColors();
+  EXPECT_EQ(allColor.size(), 8);
+}
+
+TEST(attribuite_test, class_color_test) {
+  std::vector<Color> allColor = getCellColors();
+  EXPECT_EQ(darkGray.a, allColor.front().a);
+  EXPECT_EQ(darkGray.b, allColor.front().b);
+  EXPECT_EQ(darkGray.g, allColor.front().g);
+  EXPECT_EQ(darkGray.r, allColor.front().r);
+}
